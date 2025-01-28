ca64c592fe39a11eb5fabde0eabdf31224c48b34
Milko Dinev <mjdinevpro@gmail.com>
2025-01-28 19:34:39 +0200

    Create notes component

---
+++ NotesAppClient/src/app/components/notes-list/notes-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/models/Note';
+import { NoteService } from 'src/app/services/note.service';
+
+@Component({
+  selector: 'app-notes-list',
+  templateUrl: './notes-list.component.html',
+  styleUrls: ['./notes-list.component.css']
+})
+export class NotesListComponent implements OnInit {
+  notes: Note[] = [];
+  newNote: Note = { id: 0, tile: '', description: '' };
+
+  constructor(private noteService: NoteService) {}
+
+  ngOnInit(): void {
+    this.loadNotes();
+  }
+
+  loadNotes() {
+    this.noteService.getNotes().subscribe((data) => {
+      this.notes = data;
+    });
+  }
+
+  addNotes() {
+    if (this.newNote.tile && this.newNote.description) {
+      this.noteService.addNote(this.newNote).subscribe((note) => {
+        this.notes.push(note);
+        this.newNote = { id: 0, tile: '', description: '' };
+      });
+    }
+  }
+
+  deleteNote(id: number) {
+    this.noteService.deleteNote(id).subscribe(() => {
+      this.notes = this.notes.filter((note) => note.id !== id);
+    });
+  }
+}
--- NotesAppClient/src/app/models/Note.ts
+++ NotesAppClient/src/app/models/Note.ts
@@ -1,5 +1,5 @@
 export interface Note {
     id: number;
     tile: string;
-    description: number;
+    description: string;
   }
